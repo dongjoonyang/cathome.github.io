006408c5d55a1180d675173a6c0920718e11723f
Dongjun Yang <motokrzr11@gmail.com>
2025-05-05 22:46:36 +0900

    feat : first commit

---
+++ js/catroom.js
@@ -0,0 +1,18 @@
+const tabs = document.querySelectorAll('.tab');
+const contents = document.querySelectorAll('.tab-content');
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+    const target = tab.getAttribute('data-tab');
+
+    tabs.forEach(t => t.classList.remove('active'));
+    tab.classList.add('active');
+
+    contents.forEach(c => {
+        c.classList.remove('active');
+        if (c.getAttribute('data-content') === target) {
+        c.classList.add('active');
+        }
+    });
+    });
+});
